4785db6e532dbab40db736c237652146e9f1c56f
Tuba Çulhaoğlu <tubaculhaoglu1@gmail.com>
2024-09-29 18:54:24 +0300

    Add [movieId].ts file

---
+++ pages/movies/[movieId].ts
@@ -0,0 +1,36 @@
+import prismadb from '@/lib/prismadb';
+import serverAuth from '@/lib/serverAuth';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req:NextApiRequest, res:NextApiResponse) {
+    if(req.method !== "GET"){
+        return res.status(405).end();
+    }
+    try{
+        await serverAuth(req);
+        const { movieId } = req.query;
+
+        if(typeof movieId !== 'string'){
+            throw new Error("Movie ID is not a string! Inalid ID!");
+        }
+        if(!movieId){
+            throw new Error("There is no movieID !");
+        }
+
+        const movie = await prismadb.movie.findUnique({
+            where: {
+                id: movieId,
+            }
+        });
+        if(!movie){
+            throw new Error("There is no movie with this ID!!!");
+        }
+
+        return res.status(200).json(movie);
+
+    } catch(error){
+        console.log(error);
+        return res.status(400).end();
+    }
+}
+

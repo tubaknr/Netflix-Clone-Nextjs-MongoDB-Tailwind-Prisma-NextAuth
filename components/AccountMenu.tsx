33534d3f5f151eeca68dae8ff7b979ae52fc520e
Tuba Çulhaoğlu <tubaculhaoglu1@gmail.com>
2024-09-30 11:38:55 +0300

    Fix name in AccountMenu.tsx file

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { signOut } from "next-auth/react";
+import useCurrentUser from "@/hooks/useCurrentUser";
 
 interface AccountMenuProps{
     visible?: boolean;
 }
 
 const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
+    const {data} = useCurrentUser();
+    
     if (!visible){
         return null;
     }
@@ -16,7 +19,7 @@ const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
                     <div className="px-3 group/item flex flex-row gap-3 items-center w-full">
                         <img className="w-8 rounded-md" src="/images/default_user.jpg" alt=""/>
                         <p className="text-white text-sm group-hover/item:underline">
-                        Username
+                        {data?.name}
                         </p>
                     </div>
                     <hr className="bg-gray-600 border-0 h-px my-4"></hr>
